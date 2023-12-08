fc58e7dfe0535d401f0a178e6cd17749bad8a519
piwien <berkayyasar01@gmail.com>
2023-12-08 22:09:04 +0300

    Initial Commit

---
+++ Discord-Clone/Discord_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Discord_CloneApp.swift
+//  Discord-Clone
+//
+//  Created by Berkay Yaşar on 8.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct Discord_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
